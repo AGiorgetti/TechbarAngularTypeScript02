158ff0599900a96c3e741db8f57d81b34b9239d0
Alessandro Giorgetti <alessandro.giorgetti@live.com>
2016-04-11 20:41:31 +0200

    Empty Project with TypeScript Support

---
+++ README.md
@@ -0,0 +1,55 @@
+# TechBar AngularJs and TypeScript - Moving your application to Angular 2
+
+-----------------------
+Setup With TypeScript
+-----------------------
+
+Install node: http://www.nodejs.org
+
+Install gulp: npm install gulp -g
+
+Install typings: npm install typings -g
+
+from a console prompt:
+
+npm install
+
+typings install
+
+(change Visual Studio Code settings, see below)
+
+Run Task: 'build-ts'
+
+run 'node TodoWebService.js' to start the server.
+
+browse to: http://localhost:3000/build/index.html
+
+-----------------------
+Visual Studio Code
+-----------------------
+
+if you want to use a local copy of the typescript compiler
+edit the file settings.json and specify the correct absolute path for "typescript.tsdk" 
+
+Tricks:
+
+- TypeScript Project file: if you place a *tsconfig.json* file on the root VSCode will internally pick that and use it. 
+  This way any .ts and .d.ts will be compiled and there will be no need to add /// <reference path="tsd.d.ts" /> all around.
+  No comments allowed in this file with TypeScript 1.7 and earlier versions.
+  The settings should match when you have in the build script of your task runner (gulpfile.js).
+  
+- JavaScript Project File: if you place a *jsconfig.json* file on the root VSCode will internally pick that and use it to provide some JavaScript goodies.
+
+Suggested Plugins to install (ext install ...):
+
+- tslint
+- Visual Studio Light Theme
+
+-----------------------
+Update definitions:
+-----------------------
+tsd update -s -o
+tsd rebundle
+
+ncu --upgradeAll
+npm install
---
+++ gulpfile.js
@@ -0,0 +1,92 @@
+var gulp = require("gulp");
+var ts = require("gulp-typescript");
+var sourcemaps = require("gulp-sourcemaps");
+var typescript = require("typescript");
+var tslint = require("gulp-tslint");
+var path = require("path");
+var inject = require("gulp-inject");
+//var angularFilesort = require("gulp-angular-filesort");
+
+var paths = {
+    src: "./src/", // actual source code
+    build: "./build/", // compilation artifacts (tmp)
+    dist: "./dist/" // distribution files
+}
+
+// define some compiler options
+var tsOptions = {
+    typescript: typescript, // use the local copy of the compiler
+    noImplicitAny: false,
+    target: "es5",
+    emitDecoratorMetadata: true,
+    experimentalDecorators: true,
+    experimentalAsyncFunctions: true,
+    // noExternalResolve: true, // external resolution of files is allowed, the previous commet is not valid for ng2: we provide all the file by ourselves, no <reference> is needed
+    module: "system", //"AMD" // "commonjs" // values ["AMD", "commonjs", "UMD", "system"]
+    moduleResolution: "node"
+};
+
+var tsFiles = ["typings/browser/**/*.d.ts", paths.src + "**/*.ts",];
+var source = gulp.src(tsFiles);
+
+// build with sourcemaps support
+
+gulp.task("build-ts", ["copy-templates", "compile-ts"]);
+
+gulp.task("compile-ts", function() {
+    var tsResult = source
+        .pipe(sourcemaps.init())
+        .pipe(ts(tsOptions));
+    return tsResult.js
+        //.pipe(sourcemaps.write(".", {includeContent: true})) // sourcemaps will be generated on an external file
+        //.pipe(sourcemaps.write()) // sourcemap added to the source file (does not work either works with vscode debugger)
+        .pipe(sourcemaps.write(".", { // allow VSCode debugger to work: https://github.com/ivogabe/gulp-typescript/issues/201
+            // Return relative source map root directories per file.
+            sourceRoot: function(file) {
+                var sourceFile = path.join(file.cwd, file.sourceMap.file);
+                return path.relative(path.dirname(sourceFile), file.cwd) + "/../src";  // will probide the right location of the source files relative to the build folder
+            }
+        }))
+
+        /*
+        .pipe(sourcemaps.write({ // allow VSCode debugger to work: https://github.com/ivogabe/gulp-typescript/issues/201
+          // Return relative source map root directories per file.
+          sourceRoot: function (file) {
+            var sourceFile = path.join(file.cwd, file.sourceMap.file);
+            return path.relative(path.dirname(sourceFile), file.cwd); // + "/../src";  // will probide the right location of the source files relative to the build folder
+          }
+        }))
+        */
+        .pipe(gulp.dest(paths.build));
+});
+
+gulp.task("watch", function() {
+    return gulp.watch(tsFiles, ['build-ts']);
+});
+
+gulp.task("copy-templates", function() {
+    return gulp.src(paths.src + "**/*.html")
+        .pipe(gulp.dest(paths.build));
+});
+
+// tasks not used in Angular 2 demo
+
+gulp.task("tslint", function() {
+    return source
+        .pipe(tslint())
+        .pipe(tslint.report("verbose"));
+});
+
+gulp.task("copy-js", function() {
+    return gulp.src(paths.src + "**/*.js")
+        .pipe(gulp.dest(paths.build));
+});
+
+// https://github.com/klei/gulp-inject/wiki/Clarifying-injected-paths
+gulp.task("inject", ["compile-ts"], function() {
+    return gulp.src(paths.src + "index.html")
+        .pipe(inject(
+            gulp.src([paths.build + "**/*.js"]) //.pipe(angularFilesort())
+            , { relative: true }))
+        .pipe(gulp.dest(paths.build));
+});
