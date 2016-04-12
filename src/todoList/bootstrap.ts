11cbf6485320e9576f742525f94f21374b4cf9d9
Alessandro Giorgetti <alessandro.giorgetti@live.com>
2016-04-12 14:05:29 +0200

    fixed the conversion sample

--- src/todoList/bootstrap.ts
+++ src/todoList/bootstrap.ts
@@ -5,7 +5,7 @@ import { bootstrap } from "angular2/platform/browser";
 // https://github.com/angular/angular/issues/5632#issuecomment-167026172
 // this is a demo, so we import everything!
 import "rxjs/Rx";
-import { HTTP_BINDINGS } from "angular2/http";
+import { HTTP_PROVIDERS } from "angular2/http";
 
 // import our main component and services that need to be resolved as singletons
 import { TodoList } from "./TodoList";
@@ -13,4 +13,4 @@ import { TodoListService } from "./TodoListService";
 import { InMemoryTodoListService } from "./TodoListServiceInMemory";
 
 // let everything come to life!
-bootstrap(TodoList, [HTTP_BINDINGS, TodoListService, InMemoryTodoListService]);
+bootstrap(TodoList, [HTTP_PROVIDERS, TodoListService, InMemoryTodoListService]);
